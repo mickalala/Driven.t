30bc2db24775bb5aa1c3a35570760a5c781985c8
mickalala <mickalarissa01@gmail.com>
2023-07-10 15:53:56 -0300

    payment and tickets routes autenticate user only

--- src/routers/payment-router.ts
+++ src/routers/payment-router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getPaymentTicketInfo, postPaymentTicket } from "@/controllers";
+import { authenticateToken } from "@/middlewares";
 
 const paymentRouter = Router();
 
 paymentRouter
+    .all('/*', authenticateToken)
     .get("/payments?ticketId=1", getPaymentTicketInfo)
     .post("/payments/process", postPaymentTicket);
 
--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { getTickets, getUserTicket, ticketsPost } from "@/controllers/tickets-controllers";
+import { authenticateToken } from "@/middlewares";
 
 const ticketsRouter = Router();
 
 ticketsRouter
+    .all('/*', authenticateToken)
     .get("/types", getTickets)
     .get("/", getUserTicket)
-    .post("/",ticketsPost);
+    .post("/", ticketsPost);
 
 export { ticketsRouter };
